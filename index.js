cea0e70c5f87396b0c3d0b354c0e33d74f9bcae4
Ed Salter <mayhem366@hotmail.com>
2016-09-21 22:39:05 +0100

    Added error handling

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 const app = require('express')();
 const http = require('http');
 
-function getRandomCatUrl(done) {
-  return http.get('http://thecatapi.com/api/images/get?format=src', function(response) {
+function getRandomCatUrl(err, done) {
+  var req = http.get('http://thecatapi.com/api/images/get?format=src', function(response) {
         console.log(response.headers.location);
         done(response.headers.location);
     });
+
+    req.on('error', (e) => {
+      err(e);
+    });
+
+    return req;
 }
 
 function pipeCatImage(req, res, url) {
   var connector = http.get(url, (resp) => {
     resp.pipe(res);
   });
-
+  connector.on('error', (e) => {
+    console.log(`Error occured: ${e.message}`);
+    res.send('Error');
+  });
   req.pipe(connector);
 }
 
 app.get('/', function (req, res) {
-  getRandomCatUrl((catImage) => {
+  getRandomCatUrl((e) => {
+    console.log(`Error occured: ${e.message}`);
+    res.send('Error');
+  },(catImage) => {
     pipeCatImage(req, res, catImage);
   });
 });
