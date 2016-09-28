fc5f34065b55bc52b623a3218d761d318f0ba9c3
EdwardSalter <ed@edwardsalter.me.uk>
2016-09-28 12:18:48 +0100

    Added a new command so that categories can be listed.

--- index.js
+++ index.js
@@ -1,22 +1,66 @@
 const app = require('express')();
 const http = require('http');
-
+const url = require('url');
+const querystring = require('querystring');
+const appendQuery = require('append-query');
 const config = require('./configuration');
+const xml = require('xml2js');
 
-let catApiUrl = config.get("catApiUrl") || 'http://thecatapi.com/api/images/get?format=src';
+let catApiUrl = config.get("catApiUrl") || 'http://thecatapi.com/api/';
 const catApiKey = config.get("catApiKey");
-if (catApiKey) {
-  catApiUrl += `&api_key=${catApiKey}`; // TODO: NOT STRING CONCATENATION
+console.log(`The cat api base url is ${catApiUrl}`);
+
+
+function getApiUrl(path, queryParams) {
+    let fullPath = url.resolve(catApiUrl, path);
+    if (catApiKey) {
+        fullPath = appendQuery(fullPath, { api_key: catApiKey });
+    }
+    if (queryParams) {
+        fullPath = appendQuery(fullPath, queryParams);
+    }
+    return fullPath;
 }
-console.log(`The cat api url is ${catApiUrl}`);
 
 function getRandomCatUrl(err, done) {
-  var req = http.get(catApiUrl, function(response) {
+    let imageUrl = getApiUrl('images/get');
+    console.log(`Making request to ${imageUrl}`);
+
+    let req = http.get(imageUrl, function(response) {
         done(response.headers.location);
     });
 
     req.on('error', (e) => {
       err(e);
+    });
+
+    return req;
+}
+
+function getCatCategories(err, done) {
+    let categoryUrl = getApiUrl('categories/list');
+    console.log(`Making request to ${categoryUrl}`);
+
+    let options = url.parse(categoryUrl);
+    options.headers = {
+        accept: 'applicaiton/json'
+    };
+    let req = http.get(options, function(response) {
+        var body = '';
+        response.on('data', function(d) {
+            body += d;
+        });
+        response.on('end', function() {
+            xml.parseString(body, (err, parsedXml) => {
+                done(parsedXml.response.data[0].categories[0].category.map((category) => {
+                    return category.name[0];
+                }));
+            });
+        });
+    });
+
+    req.on('error', (e) => {
+    err(e);
     });
 
     return req;
@@ -34,22 +78,43 @@ function pipeCatImage(req, res, url) {
 }
 
 app.get('/', function(req, res) {
-  getRandomCatUrl((e) => {
-    console.log(`Error occured getting image url from cat api: ${e.message}`);
-    res.status(500).send('Error');
-  },(catImage) => {
-    let json = {
-      "response_type": "in_channel",
-      "attachments": [
-          {
-              "fallback": "A random cat",
-              "pretext": "Your random cat",
-              "image_url": catImage
-          }
-      ]
-    };
-    res.json(json);
-  });
+    let body = req.query;
+
+    var command = body.text ? body.text.split(" ")[0].toLowerCase() : null;
+
+    switch (command) {
+        case 'categories':
+            getCatCategories((e) => {
+              console.log(`Error occured getting category list from cat api: ${e.message}`);
+              res.status(500).send('Error');
+          }, (categories) => {
+              let json = {
+                "response_type": "ephemeral",
+                "text": `The categories you can choose are: ${categories.join(', ')}`
+              };
+              res.json(json);
+          })
+            break;
+
+        default:
+            getRandomCatUrl((e) => {
+              console.log(`Error occured getting image url from cat api: ${e.message}`);
+              res.status(500).send('Error');
+            },(catImage) => {
+              let json = {
+                "response_type": "in_channel",
+                "attachments": [
+                    {
+                        "fallback": "A random cat",
+                        "pretext": "Your random cat",
+                        "image_url": catImage
+                    }
+                ]
+              };
+              res.json(json);
+            });
+            break;
+    }
 })
 
 app.get('/image', function (req, res) {
