bd435ce83d1705b03164896fed122ce10e6178bf
Edward Salter <edward.salter@microlise.com>
2018-12-31 14:36:36 +0000

    Reenabled slack endpoint

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { searchImages } from "./catApiClient";
 import config from "./configuration";
 import restify from "restify";
 import { BotFrameworkAdapter } from "botbuilder";
+import sendSlackResponse from "./SlackResponse";
 
 const botFrameworkConfig = config.get("azureBotFramework");
 
@@ -59,72 +60,72 @@ server.post("/api/messages", (req, res) => {
 //   });
 //   req.pipe(connector);
 // }
+
+async function getImage() {
+  const images = await searchImages({ size: "small" });
+  console.debug("Got images back");
+  const message = formatImageMessage(images[0]);
+  return message;
+}
+
+// Slack command
+server.post("/", async function(req, res) {
+  const message = await sendSlackResponse(getImage(), req.body.response_url);
+
+  console.debug("Got a message to send", message);
+
+  res.json(message);
+});
+
+function formatImageMessage(image) {
+  return {
+    response_type: "in_channel",
+    attachments: [
+      {
+        fallback: "A random cat",
+        pretext: "Your random cat",
+        image_url: image.url
+      }
+    ]
+  };
+}
 //
-// async function getImage() {
-//   const images = await searchImages({ size: "small" });
-//   console.debug("Got images back");
-//   const message = formatImageMessage(images[0]);
-//   return message;
-// }
-//
-// // Slack command
-// app.post("/", async function(req, res) {
-//   const message = await sendSlackResponse(getImage(), req.body.response_url);
-//
-//   console.debug("Got a message to send", message);
-//
-//   res.json(message);
+// server.post("/image/:id?", function(req, res) {
+//   getRandomCat(
+//     null,
+//     req.params.id,
+//     e => {
+//       console.log(`Error occured getting image url from cat api: ${e.message}`);
+//       res.status(500).send(e.message);
+//     },
+//     catImage => {
+//       pipeCatImage(req, res, catImage.url);
+//     }
+//   );
 // });
 //
-// function formatImageMessage(image) {
-//   return {
-//     response_type: "in_channel",
-//     attachments: [
-//       {
-//         fallback: "A random cat",
-//         pretext: "Your random cat",
-//         image_url: image.url
-//       }
-//     ]
-//   };
-// }
+// server.post("/", function(req, res) {
+//   getRandomCat(
+//     null,
+//     null,
+//     e => {
+//       console.log(`Error occured getting image url from cat api: ${e.message}`);
+//       res.status(500).send(e.message);
+//     },
+//     catImage => {
+//       const fullUrl =
+//         req.protocol + "://" + req.get("host") + "/image/" + catImage.id;
+//       let json = {
+//         message: '<img src="' + fullUrl + '" />',
+//         notify: true,
+//         message_format: "html"
+//       };
 //
-// // app.post("/image/:id?", function(req, res) {
-// //   getRandomCat(
-// //     null,
-// //     req.params.id,
-// //     e => {
-// //       console.log(`Error occured getting image url from cat api: ${e.message}`);
-// //       res.status(500).send(e.message);
-// //     },
-// //     catImage => {
-// //       pipeCatImage(req, res, catImage.url);
-// //     }
-// //   );
-// // });
-//
-// // app.post("/", function(req, res) {
-// //   getRandomCat(
-// //     null,
-// //     null,
-// //     e => {
-// //       console.log(`Error occured getting image url from cat api: ${e.message}`);
-// //       res.status(500).send(e.message);
-// //     },
-// //     catImage => {
-// //       const fullUrl =
-// //         req.protocol + "://" + req.get("host") + "/image/" + catImage.id;
-// //       let json = {
-// //         message: '<img src="' + fullUrl + '" />',
-// //         notify: true,
-// //         message_format: "html"
-// //       };
-// //
-// //       res.json(json);
-// //     }
-// //   );
-// // });
-//
+//       res.json(json);
+//     }
+//   );
+// });
+
 // const port = config.get("port", null, false) || 3000;
 // app.listen(port, function() {
 //   console.log("slack-random-cat listening on port " + port);
